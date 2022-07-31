7c745f7877044c8e1466f1a14fb38da6d478245a
小王同学 <104604696+w522949722@users.noreply.github.com>
2022-07-31 23:32:14 +0800

    Add files via upload

--- blog.js
+++ blog.js
@@ -9,13 +9,13 @@ const blog = [
   {
     id: 2,
     title: "我的召唤兽",
-    herf: "https://w522949722.github.io/01changku/pageZHS",
+    herf: "https://www.xiaowangtongxue.cn/pageZHS",
     image: "https://s1.328888.xyz/2022/07/24/mWkdB.jpg",
   },
   {
     id: 3,
     title: "我的爱好",
-    herf: "https://w522949722.github.io/01changku/paopao",
+    herf: "https://www.xiaowangtongxue.cn/paopao",
     image: "https://s1.328888.xyz/2022/07/24/mdAZk.jpg",
   },
 ];
