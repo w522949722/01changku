c982d4bdb0006a4ec9f30b8cd630a0c9e3329b16
小王同学 <104604696+w522949722@users.noreply.github.com>
2022-11-28 17:18:52 +0800

    Add files via upload

--- blog.js
+++ blog.js
@@ -4,18 +4,18 @@ const blog = [
     id: 1,
     title: "我的学校",
     herf: "https://www.midorg.com/",
-    image: "./tu01.jpg",
+    image: "tu01.jpg",
   },
   {
     id: 2,
     title: "我的召唤兽",
     herf: "https://www.xiaowangtongxue.cn/pageZHS",
-    image: "./tu02.jpg",
+    image: "tu02.jpg",
   },
   {
     id: 3,
     title: "我的爱好",
     herf: "https://www.xiaowangtongxue.cn/paopao",
-    image: "./tu03.jpg",
+    image: "tu03.jpg",
   },
 ];
