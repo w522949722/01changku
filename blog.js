ad48de8e0168ff574985a2ce73a5c940e6ef09aa
小王同学 <104604696+w522949722@users.noreply.github.com>
2022-11-28 17:04:37 +0800

    Add files via upload

--- blog.js
+++ blog.js
@@ -4,18 +4,18 @@ const blog = [
     id: 1,
     title: "我的学校",
     herf: "https://www.midorg.com/",
-    image: "https://s1.328888.xyz/2022/07/24/mWatU.jpg",
+    image: "./tu01.jpg",
   },
   {
     id: 2,
     title: "我的召唤兽",
     herf: "https://www.xiaowangtongxue.cn/pageZHS",
-    image: "https://s1.328888.xyz/2022/07/24/mWkdB.jpg",
+    image: "./tu02.jpg",
   },
   {
     id: 3,
     title: "我的爱好",
     herf: "https://www.xiaowangtongxue.cn/paopao",
-    image: "https://s1.328888.xyz/2022/07/24/mdAZk.jpg",
+    image: "./tu03.jpg",
   },
 ];
